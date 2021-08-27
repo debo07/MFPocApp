3bcac0feb0b96d72283dafeb78e8179e5955259c
Debajit Majumder <debajit.business@gmail.com>
2021-08-27 20:20:21 +0530

    html id corrected and render remote app in the host container app

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,6 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as productMount } from "products/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
 
 console.log("This is container");
+productMount(document.querySelector("#products-list"));
+cartMount(document.querySelector("#cart-section"));
