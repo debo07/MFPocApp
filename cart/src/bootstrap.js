54bb13b801250a0e4f43a638d1cb52527298c8ff
Debajit Majumder <debajit.business@gmail.com>
2021-08-27 20:16:09 +0530

    product and cart app changed so that they are not dependent on their own html. Their own html will be used for testing the standalone app. Host app can pass its own element where it will be rendered

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,18 @@
 import faker from "faker";
 
-const cartText = `<div>Total cart items ${faker.random.number(10)}</div>`;
+const mount = el => {
+    const cartText = `<div>Total cart items ${faker.random.number(10)}</div>`;
+    el.innerHTML = cartText;
+}
 
-document.querySelector('#cart-section').innerHTML = cartText;
+// Standalone mode - where this "cart" app will be tested separately.
+if(process.env.NODE_ENV === "development") {
+    const localElement = document.querySelector("#local-cart-section");
+    // Assuming div with "local-cart-section" id won't be available in the host app.
+    if(localElement) {
+        mount(localElement);
+    }
+}
+
+// Where it will be used by Host app, they will pass their root element
+export { mount };
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,22 @@
 import faker from "faker";
 
-let products = "";
+const mount = el => {
+    let products = "";
 
-for (let index = 0; index < 5; index++) {
-  const productName = faker.commerce.productName();
-  products += `<div>${productName}</div>`;
+    for (let index = 0; index < 5; index++) {
+        const productName = faker.commerce.productName();
+        products += `<div>${productName}</div>`;
+    }
+
+    el.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === "development") {
+    const localElement = document.querySelector("#local-products-list");
+    // Assuming div with "local-products-list" id won't be available in the host app.
+    if (localElement) {
+        mount(localElement);
+    }
 }
 
-document.querySelector("#products-list").innerHTML = products;
+export { mount };
