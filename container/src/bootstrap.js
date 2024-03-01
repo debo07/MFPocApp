3063e46827ed16e66b1afca8b66f23fb6cf44b5d
Debajit Majumder <debajit.business@gmail.com>
2024-03-02 00:16:14 +0530

    Tried out 2 different APIs, 1 from MFE and 1 from container. Chrome is showing some issues at times, where 1 worker is getting detached, need more testing. Worked fine on Firefox.

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -6,6 +6,7 @@ import worker from "../mocks/server";
 
 console.log("This is container");
 async function enableSelfMocking() {
+    return worker.start();
     return worker.start({
         serviceWorker: {
             url: "/mockServiceWorkerSelf.js",
@@ -13,12 +14,28 @@ async function enableSelfMocking() {
     });
 }
 
+const fetchData = async () => {
+    try {
+        const response = await fetch("/api/data1");
+        const data = await response.json();
+        console.log("Data for /data1:", data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+};
+
 enableSelfMocking().then(() => {
     console.log("Self mocking enabled");
-    enableMocking("/mockServiceWorkerFeedback.js").then(() => {
-        console.log("Remote Mocking enabled");
-        productMount(document.querySelector("#products-list"));
-        cartMount(document.querySelector("#cart-section"));
-        feedbackMount(document.querySelector("#feedback-section"));
+    fetchData().then(data => {
+        const el = document.querySelector("#data-section");
+        el.innerHTML = JSON.stringify(data, null, 2);
     });
+});
+
+enableMocking("/mockServiceWorkerFeedback.js").then(() => {
+    console.log("Remote Mocking enabled");
+    productMount(document.querySelector("#products-list"));
+    cartMount(document.querySelector("#cart-section"));
+    feedbackMount(document.querySelector("#feedback-section"));
 });
